e25401bf537121ee53cd557375b5b7adb07b354a
GaboXandre <gabo.xandre@gmail.com>
2015-02-20 13:21:10 -0300

    v 0.1

---
+++ README.md
@@ -0,0 +1,32 @@
+# HiddenDoors
+
+Imagine hiding a secret room. The door is invisible unless you know where to look...
+HiddenDoors uses default textures to create walkable (and climbable) trapdoors.
+
+Included:
+* Dirt
+* Wood
+* Tree
+* Brick
+* Stone
+* Stone Brick
+* Desert Stone
+* Desert Stone Brick
+* Obsidian
+
+In the inventory, the texture is surrounded by a yellow frame to distinguish from the real nodes. But once placed they look identical.
+
+## Dependencies
+* defualt
+
+## Credits
+Textures are taken from default mod. Credits can be found in the readme.txt included with default mod.
+Inventory images are slightly modified versions.
+
+### License of media (textures)
+Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+http://creativecommons.org/licenses/by-sa/3.0/
+
+### License of source code:
+[WTFPL](http://www.wtfpl.net/)
+
---
+++ hiddendoors/init.lua
@@ -0,0 +1,34 @@
+-- HiddenDoors
+
+hiddendoors = {}
+
+function hiddendoors.register_door(material, name)
+	minetest.register_node("hiddendoors:" .. material, {
+	description = name .. " Hidden Door",
+	walkable = false,
+	climbable = true,
+	is_ground_content = false,
+	inventory_image = "hiddendoors_" .. material .. "_inventory.png",
+	tiles = {"hiddendoors_" .. material .. ".png"},
+	groups = {cracky = 2},
+	})
+	
+	minetest.register_craft({
+		output = "hiddendoors:" .. material .. " 2",
+		recipe = {
+		{"", "", ""},
+		{"default:glass", "default:" .. material , ""},
+		{"", "", ""}
+		}
+	})
+end
+
+hiddendoors.register_door("obsidian", "Obsidian")
+hiddendoors.register_door("stone", "Stone")
+hiddendoors.register_door("desert_stone_brick", "Desert Stone Brick")
+hiddendoors.register_door("stone_brick", "Stone Brick")
+hiddendoors.register_door("tree", "Tree")
+hiddendoors.register_door("dirt", "Dirt")
+hiddendoors.register_door("desert_stone", "Desert Stone")
+hiddendoors.register_door("brick", "Brick")
+hiddendoors.register_door("wood", "Wood")
