694285d4472b3088d34dc0b9897cb53437d28925
nanepiwo <spamdumpingstation@gmail.com>
2015-03-10 22:33:36 -0400

    Improvements
    Make compatible with mods other than default, add texture field for function. Your present :). I hope I used the if else for tiles correctly.

--- hiddendoors/init.lua
+++ hiddendoors/init.lua
@@ -2,33 +2,38 @@
 
 hiddendoors = {}
 
-function hiddendoors.register_door(material, name)
-	minetest.register_node("hiddendoors:" .. material, {
+function hiddendoors.register_door(modname, nodename, description, texture) -- texture is optional, for when a mod does not follow naming conventions.
+	minetest.register_node("hiddendoors:" .. nodename .. "_" .. modname, {
 	description = name .. " Hidden Door",
 	walkable = false,
 	climbable = true,
 	is_ground_content = false,
 	inventory_image = "hiddendoors_" .. material .. "_inventory.png",
-	tiles = {"default_" .. material .. ".png"},
+	tiles = 
+		if texture == nil then
+			{modname .. "_" .. material .. ".png"},
+		else
+			{texture}
+		end
 	groups = {cracky = 2},
 	})
 	
 	minetest.register_craft({
-		output = "hiddendoors:" .. material .. " 2",
+		output = "hiddendoors:" .. nodename .. "_" .. modname  "2",
 		recipe = {
 		{"", "", ""},
-		{"default:glass", "default:" .. material , ""},
+		{"default:glass", modname .. ":" .. nodename , ""},
 		{"", "", ""}
 		}
 	})
 end
 
-hiddendoors.register_door("obsidian", "Obsidian")
-hiddendoors.register_door("stone", "Stone")
-hiddendoors.register_door("desert_stone_brick", "Desert Stone Brick")
-hiddendoors.register_door("stone_brick", "Stone Brick")
-hiddendoors.register_door("tree", "Tree")
-hiddendoors.register_door("dirt", "Dirt")
-hiddendoors.register_door("desert_stone", "Desert Stone")
-hiddendoors.register_door("brick", "Brick")
-hiddendoors.register_door("wood", "Wood")
+hiddendoors.register_door("default", "obsidian", "Obsidian")
+hiddendoors.register_door("default", "stone", "Stone")
+hiddendoors.register_door("default", "desert_stone_brick", "Desert Stone Brick")
+hiddendoors.register_door("default", "stone_brick", "Stone Brick")
+hiddendoors.register_door("default", "tree", "Tree")
+hiddendoors.register_door("default", "dirt", "Dirt")
+hiddendoors.register_door("default", "desert_stone", "Desert Stone","default_desert_stone_brick.png")
+hiddendoors.register_door("default", "brick", "Brick")
+hiddendoors.register_door("default", "wood", "Wood")
